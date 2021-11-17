63d2ed224da851716a062e27c089d119a6152c76
MohamedSolimann <mohamadico@gmail.com>
2021-11-17 15:48:47 +0200

    fixed pagination endpoint bug in product routes

--- routes/products/index.js
+++ routes/products/index.js
@@ -4,13 +4,23 @@ const productModel = require("../../models/product/product.schema");
 
 Router.get("/:category/:page/:limit", async (req, res) => {
   let { category, page, limit } = req.params;
+  let pageAsNumber = parseInt(page);
+  let limitAsNumber = parseInt(limit);
   try {
     let products = await productModel.find({
       category,
       deletedDate: undefined,
     });
-    let productsToBeDisplayed = products.splice((page - 1) * 5, limit);
-    res.status(200).json({ message: "Success", data: productsToBeDisplayed });
+    let productsLength = products.length;
+    let productsToBeDisplayed = products.splice(
+      (pageAsNumber - 1) * limitAsNumber,
+      limitAsNumber
+    );
+    if ((pageAsNumber - 1) * limitAsNumber + limitAsNumber >= productsLength) {
+      res.status(200).json({ message: "No More", data: productsToBeDisplayed });
+    } else {
+      res.status(200).json({ message: "Success", data: productsToBeDisplayed });
+    }
   } catch (error) {
     res.status(500).json({ message: "Error" });
   }
